1abaafb79cedd835f64b02b871d0e8553603f5fc
Ritesh-HangingPanda <ritesh.hp.sharma@gmail.com>
2025-03-20 13:06:15 +0530

    update rekognitionClient to use dynamic region from environment variables

--- src/service/rekognitionClient.js
+++ src/service/rekognitionClient.js
@@ -1,8 +1,8 @@
 import { RekognitionClient } from "@aws-sdk/client-rekognition";
-const { ACCESS_KEY_ID, SECRET_ACCESS_KEY } = import.meta.env;
+const { REGION_NAME, ACCESS_KEY_ID, SECRET_ACCESS_KEY } = import.meta.env;
 
 const rekognitionClient = new RekognitionClient({
-  region: "us-east-1",
+  region: REGION_NAME,
   credentials: {
     accessKeyId: ACCESS_KEY_ID,
     secretAccessKey: SECRET_ACCESS_KEY,
