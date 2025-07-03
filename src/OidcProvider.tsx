0477668f737606efc54ef109e6af503c2f42922c
Vikas Yadav <vikasyadav01.hp@outlook.com>
2025-07-03 11:33:07 +0530

    Update OIDC authority URL in OidcProvider configuration

--- src/OidcProvider.tsx
+++ src/OidcProvider.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AuthProvider } from "react-oidc-context";
 
 const oidcConfig = {
-    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_CFAYyT5hN", // Your Cognito domain
+    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_CFAoyTt3h", // Your Cognito domain
     client_id: "7hascthfrhhr9sgmfta53kjpu", // Your app client ID
     redirect_uri: window.location.origin, // Use current origin for redirect
     response_type: "code",
